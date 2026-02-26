c9fd22906afbc618559f2b1523baf1886ceded3f
Drjackxiaoyuchen <111806807+Drjackxiaoyuchen@users.noreply.github.com>
2026-02-26 06:08:55 +0000

    Deploying to gh-pages from @ Drjackxiaoyuchen/drjackxiaoyuchen.github.io@32a7f7d601fa85c4b424d8905bb39b213a40a322 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,6 +63,186 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_emperor_of_all_maladies/";
+            },},{id: "books-a-life-on-our-planet-my-witness-statement-and-a-vision-for-the-future",
+          title: 'A Life on Our Planet: My Witness Statement and a Vision for the...',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/a_life_on_our_planet/";
+            },},{id: "books-a-planet-of-viruses",
+          title: 'A Planet of Viruses',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/a_planet_of_viruses/";
+            },},{id: "books-chaos-making-a-new-science",
+          title: 'Chaos: Making a New Science',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/chaos/";
+            },},{id: "books-exhalation-stories",
+          title: 'Exhalation: Stories',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/exhalation/";
+            },},{id: "books-first-love-last-rites",
+          title: 'First Love, Last Rites',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/first_love_last_rites/";
+            },},{id: "books-genome-the-autobiography-of-a-species-in-23-chapters",
+          title: 'Genome: The Autobiography of a Species in 23 Chapters',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/genome/";
+            },},{id: "books-guns-germs-and-steel-the-fates-of-human-societies",
+          title: 'Guns, Germs, and Steel: The Fates of Human Societies',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/guns_germs_and_steel/";
+            },},{id: "books-immune-a-journey-into-the-mysterious-system-that-keeps-you-alive",
+          title: 'Immune: A Journey into the Mysterious System That Keeps You Alive',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/immune/";
+            },},{id: "books-life-3-0-being-human-in-the-age-of-artificial-intelligence",
+          title: 'Life 3.0: Being Human in the Age of Artificial Intelligence',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/life_3_0/";
+            },},{id: "books-life-on-the-edge-the-coming-of-age-of-quantum-biology",
+          title: 'Life on the Edge: The Coming of Age of Quantum Biology',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/life_on_the_edge/";
+            },},{id: "books-machines-of-loving-grace",
+          title: 'Machines of Loving Grace',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/machines_of_loving_grace/";
+            },},{id: "books-our-mathematical-universe-my-quest-for-the-ultimate-nature-of-reality",
+          title: 'Our Mathematical Universe: My Quest for the Ultimate Nature of Reality',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/our_mathematical_universe/";
+            },},{id: "books-parasite-rex-inside-the-bizarre-world-of-nature-39-s-most-dangerous-creatures",
+          title: 'Parasite Rex: Inside the Bizarre World of Nature&amp;#39;s Most Dangerous Creatures',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/parasite_rex/";
+            },},{id: "books-probable-impossibilities-musings-on-beginnings-and-endings",
+          title: 'Probable Impossibilities: Musings on Beginnings and Endings',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/probable_impossibilities/";
+            },},{id: "books-rapport-the-four-ways-to-read-people",
+          title: 'Rapport: The Four Ways to Read People',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/rapport/";
+            },},{id: "books-selected-works-of-mao-zedong",
+          title: 'Selected Works of Mao Zedong',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/selected_works_of_mao_zedong/";
+            },},{id: "books-seven-brief-lessons-on-physics",
+          title: 'Seven Brief Lessons on Physics',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/seven_brief_lessons/";
+            },},{id: "books-six-not-so-easy-pieces-einstein-39-s-relativity-symmetry-and-space-time",
+          title: 'Six Not-So-Easy Pieces: Einstein&amp;#39;s Relativity, Symmetry and Space-Time',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/six_not_so_easy_pieces/";
+            },},{id: "books-skin-in-the-game-hidden-asymmetries-in-daily-life",
+          title: 'Skin in the Game: Hidden Asymmetries in Daily Life',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/skin_in_the_game/";
+            },},{id: "books-snow-crash",
+          title: 'Snow Crash',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/snow_crash/";
+            },},{id: "books-the-almanack-of-naval-ravikant-a-guide-to-wealth-and-happiness",
+          title: 'The Almanack of Naval Ravikant: A Guide to Wealth and Happiness',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_almanack_of_naval_ravikant/";
+            },},{id: "books-the-bed-of-procrustes-philosophical-and-practical-aphorisms",
+          title: 'The Bed of Procrustes: Philosophical and Practical Aphorisms',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_bed_of_procrustes/";
+            },},{id: "books-the-beginning-of-infinity-explanations-that-transform-the-world",
+          title: 'The Beginning of Infinity: Explanations That Transform the World',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_beginning_of_infinity/";
+            },},{id: "books-the-creation-of-inequality-how-our-prehistoric-ancestors-set-the-stage-for-monarchy-slavery-and-empire",
+          title: 'The Creation of Inequality: How Our Prehistoric Ancestors Set the Stage for Monarchy,...',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_creation_of_inequality/";
+            },},{id: "books-the-evolution-of-everything-how-new-ideas-emerge",
+          title: 'The Evolution of Everything: How New Ideas Emerge',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_evolution_of_everything/";
+            },},{id: "books-the-hitchhiker-39-s-guide-to-the-galaxy",
+          title: 'The Hitchhiker&amp;#39;s Guide to the Galaxy',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_hitchhikers_guide/";
+            },},{id: "books-the-nature-of-technology-what-it-is-and-how-it-evolves",
+          title: 'The Nature of Technology: What It Is and How It Evolves',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_nature_of_technology/";
+            },},{id: "books-the-origins-of-virtue-human-instincts-and-the-evolution-of-cooperation",
+          title: 'The Origins of Virtue: Human Instincts and the Evolution of Cooperation',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_origins_of_virtue/";
+            },},{id: "books-the-rational-optimist-how-prosperity-evolves",
+          title: 'The Rational Optimist: How Prosperity Evolves',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_rational_optimist/";
+            },},{id: "books-the-singularity-is-nearer",
+          title: 'The Singularity Is Nearer',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_singularity_is_nearer/";
+            },},{id: "books-the-vital-question-energy-evolution-and-the-origins-of-complex-life",
+          title: 'The Vital Question: Energy, Evolution, and the Origins of Complex Life',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_vital_question/";
+            },},{id: "books-the-wandering-earth",
+          title: 'The Wandering Earth',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_wandering_earth/";
+            },},{id: "books-the-way-life-works",
+          title: 'The Way Life Works',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_way_life_works/";
+            },},{id: "books-the-world-as-i-see-it",
+          title: 'The World as I See It',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_world_as_i_see_it/";
+            },},{id: "books-thinking-fast-and-slow",
+          title: 'Thinking, Fast and Slow',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/thinking_fast_and_slow/";
+            },},{id: "books-what-we-cannot-know-explorations-at-the-edge-of-knowledge",
+          title: 'What We Cannot Know: Explorations at the Edge of Knowledge',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/what_we_cannot_know/";
             },},{id: "news-selected-as-one-of-100-leaders-of-tomorrow-at-the-gap-summit-2020-biotech-conference-in-cambridge-uk",
           title: 'Selected as one of 100 Leaders of Tomorrow at the Gap Summit 2020...',
           description: "",
